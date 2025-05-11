dceea525db4bff3a935d3f177dfadf6c19e7a0f6
Abir-dev <abirlalbanerjee44@gmail.com>
2025-05-11 21:59:50 +0530

    Fixing the issue

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,9 +1,8 @@
-import { useRef, useState, lazy } from "react";
+import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 import TitleHeader from "../components/TitleHeader";
-const ContactExperience = lazy(() => import("../components/Models/contact/ContactExperience"));
-
+// import ContactExperience from "../components/Models/contact/ContactExperience.jsx";
 
 const Contact = () => {
     const formRef = useRef(null);
